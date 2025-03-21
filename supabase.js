db14e2d87fd377b5014bf6f61d19fa3d927e46ea
steve-rops <somebiss@gmail.com>
2025-03-21 22:06:39 +0200

    changed the way data being fetched

--- supabase.js
+++ supabase.js
@@ -1,17 +1,22 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-);
-
 export const getEvents = async () => {
   try {
-    const { data, error } = await supabase.from("events").select("*");
-    if (error) throw new Error(error.message);
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_SUPABASE_URL}/rest/v1/events`,
+      {
+        headers: {
+          apiKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+          Authorization: `Bearer ${process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY}`,
+        },
+        cache: "force-cache", // Cache the request
+        next: { revalidate: 10 }, // Revalidate every 60 seconds
+      }
+    );
 
-    return data;
+    if (!response.ok) throw new Error("Failed to fetch events");
+
+    return await response.json();
   } catch (error) {
     console.error(error);
+    return [];
   }
 };
