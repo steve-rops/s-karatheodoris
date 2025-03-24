fd07d2d59cf44f23058156ed9bc2e58d68de5bd9
steve-rops <somebiss@gmail.com>
2025-03-24 19:55:51 +0200

    every event has its own page

---
+++ app/(pages)/ekdilosis/[title]/page.js
@@ -0,0 +1,19 @@
+import { getEvents } from "@/supabase";
+import { Suspense } from "react";
+
+export default async function SingleEkdilosiPage({ params }) {
+  const para = await params;
+  const slug = para.title;
+
+  const data = await getEvents();
+  const event = [...data.mainEvents, ...data.secEvents].find(
+    (ev) => ev.slug === slug
+  );
+
+  console.log(slug);
+  return (
+    <Suspense fallback={<div className="loader"></div>}>
+      <h1 className="text-2xl text-primary text-center">{event.title}</h1>
+    </Suspense>
+  );
+}
