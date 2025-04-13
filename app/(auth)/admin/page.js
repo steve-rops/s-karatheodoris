d381c90d57885d09a4f0f763556185f4ec4e3743
steve-rops <somebiss@gmail.com>
2025-04-13 21:09:14 +0300

    deleted the admin fetch

--- app/(auth)/admin/page.js
+++ app/(auth)/admin/page.js
@@ -1,8 +1,3 @@
-import { getEvents } from "@/supabase";
-
 export default async function AdminPage() {
-  const { mainEvents, secEvents } = await getEvents();
-  const data = [...mainEvents, ...secEvents];
-  console.log(data);
   return <div className=""></div>;
 }
