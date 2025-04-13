6fe446e533a8cf8f87a057b1f744864f558092be
steve-rops <somebiss@gmail.com>
2025-04-13 12:13:38 +0300

    minor UI/UX changes in ekdilosi list

--- app/(pages)/ekdilosis/page.js
+++ app/(pages)/ekdilosis/page.js
@@ -4,7 +4,7 @@ export const dynamic = "force-dynamic";
 
 export default function EkdilosisPage() {
   return (
-    <div className="lg:max-w-[65%] lg:mx-auto space-y-10">
+    <div className="lg:max-w-[75%] lg:mx-auto space-y-10">
       <h1 className="text-2xl text-primary text-center">Εκδηλώσεις</h1>
       {/* 
       <Suspense fallback={<EkdilosisListSkeleton />}>
